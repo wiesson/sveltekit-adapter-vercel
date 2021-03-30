6d34a6d5a9457de32261be81090adf1137bec3ef
Arne Wiese <arne@wiese.me>
2021-03-30 15:47:20 +0200

    Update vercel adapter

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
-const { writeFileSync, mkdirSync, renameSync, copyFileSync, existsSync, statSync, readdirSync } = require('fs');
-const { resolve, join, basename, dirname } = require('path');
-
-/** @param {string} dir */
-function mkdirp(dir) {
-	try {
-		mkdirSync(dir, { recursive: true });
-	} catch (e) {
-		if (e.code === 'EEXIST') return;
-		throw e;
-	}
-}
-
-/**
- * @param {string} from
- * @param {string} to
- * @param {(basename: string) => boolean} filter
- */
-function copy(from, to, filter = () => true) {
-	if (!existsSync(from)) return [];
-	if (!filter(basename(from))) return [];
-
-	const files = [];
-	const stats = statSync(from);
-
-	if (stats.isDirectory()) {
-		readdirSync(from).forEach((file) => {
-			files.push(...copy(join(from, file), join(to, file)));
-		});
-	} else {
-		mkdirp(dirname(to));
-		copyFileSync(from, to);
-		files.push(to);
-	}
-
-	return files;
-}
+const { writeFileSync, mkdirSync, renameSync } = require('fs');
+const { resolve, join } = require('path');
 
 module.exports = function () {
 	/** @type {import('@sveltejs/kit').Adapter} */
@@ -56,7 +21,7 @@ module.exports = function () {
 			builder.copy_server_files(server_directory);
 			renameSync(join(server_directory, 'app.js'), join(server_directory, 'app.mjs'));
 
-			copy(join(__dirname, 'files'), lambda_directory);
+			builder.copy(join(__dirname, 'files'), lambda_directory);
 
 			builder.log.minor('Prerendering static pages...');
 			await builder.prerender({
